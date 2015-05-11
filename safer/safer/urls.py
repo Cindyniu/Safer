9ee583d098c1cc108b67de5d4542d4ad3ec33983
cindyniu <cindyniu@cindyniu.com>
2015-05-11 15:31:50 +0800

    random show facial expression image.

--- safer/safer/urls.py
+++ safer/safer/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from safer.views import hello, camera_capture
 
 urlpatterns = patterns('',
     (r'^hello/$', hello),
     (r'^camera/$', camera_capture),
-)
+)   
+
+#add static urlpatterns
+urlpatterns += staticfiles_urlpatterns()
